535afc17f9dc9b3bf3676f34572e006cfc7b39ea
xuxinlei111 <152256455+xuxinlei111@users.noreply.github.com>
2023-11-28 18:01:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+let ws = new WebSocket("wss://one23-ze35.onrender.com/:443");
 
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
